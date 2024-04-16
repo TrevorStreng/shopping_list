e817850c75900d5e259eba5d2734d52cbf805373
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2024-04-15 22:29:53 -0700

    fixed part of login page and added submit button. Need to finish creating user routes

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,13 +1,23 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+const username = ref("");
+const password = ref("");
+
+const submit = () => {
+  console.log(username);
+  console.log(password);
+};
+</script>
 
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center h-full">
     <div class="flex flex-col">
       <h1 class="text-center text-xl font-xl">Welcome</h1>
-      <p>Username: {{ username }}</p>
+      <p>Username:</p>
       <input v-model="username" class="border border-black rounded" />
-      <p>Password: {{ password }}</p>
+      <p>Password:</p>
       <input v-model="password" class="border border-black rounded" />
+      <button @click="submit" class="border rounded">submit</button>
     </div>
   </div>
 </template>
