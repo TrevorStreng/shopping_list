339d7d6ab07b0cdf4531db9aa407cdcebaffc422
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2024-04-25 16:08:41 -0700

    finished createUser method working on login method

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -40,15 +40,64 @@ namespace backend.Controllers
       }
     }
 
+    public async Task<bool> CheckUsername(string username) {
+        string usernameQuery = "SELECT * FROM Users WHERE username = @username";
+
+        var existingUser = await _dbConnection.QueryFirstOrDefaultAsync<User>(usernameQuery, new {Username = username});
+
+        if(existingUser != null) return true;
+        return false;
+    }
+    public async Task<bool> CheckEmail(string email) {
+        string emailQuery = "SELECT * FROM Users WHERE email = @email";
+
+        var existingUser = await _dbConnection.QueryFirstOrDefaultAsync<User>(emailQuery, new {Email = email});
+
+        if(existingUser != null) return true;
+        return false;
+    }
+
     [HttpPost]
+    [Route("CreateUser")]
     public async Task<ActionResult> CreateUser(User user) {
-      // try {
-        System.Console.WriteLine(user);
-        string query = "INSERT INTO Users (username, password, email) values (@username, @password, @email);";
-        await _dbConnection.ExecuteScalarAsync(query, user);
-        // string hashedPassword = _passwordHashingService.HashPassword(user);
-        return Ok(user);
-      // }
+      try {
+        // check if email and password are in use
+        if(await CheckUsername(user.UserName!)) return BadRequest(user.UserName + "Username is already in use!");
+        if(await CheckEmail(user.Email!)) return BadRequest(user.Email + "Email is already in use!");
+
+        // hash password
+        string hashedPassword = _passwordHashingService.HashPassword(user.PasswordHash!);
+
+        string query = "INSERT INTO Users (username, password, email) values (@UserName, @PasswordHash, @Email);";
+        var newUser = new User {UserName = user.UserName, PasswordHash = hashedPassword, Email = user.Email};
+        await _dbConnection.ExecuteAsync(query, newUser);
+
+        return Ok(newUser);
+      } catch(Exception ex) {
+        return BadRequest(ex.Message);
+      }
+    }
+
+    [HttpPost]
+    [Route("Login")]
+    public async Task<ActionResult> Login(User req) {
+      try {
+        string query = "SELECT * FROM Users WHERE Username = @Username;";
+        var user = await _dbConnection.QuerySingleOrDefaultAsync(query, new {Username = req.UserName});
+        if(user == null) return BadRequest("Invalid username.🤬");
+
+        System.Console.WriteLine(req.UserName + " " + user.UserName);
+        
+        // bool passwordCheck = _passwordHashingService.VerifyPassword(user.password, req.PasswordHash);
+
+        // if(!passwordCheck) return BadRequest("Invalid Password..");
+
+        // TODO: If password check passes create and send sign in token
+
+        return Ok(user + " logged in");
+      } catch(Exception ex) {
+        return BadRequest(ex.Message);
+      }
     }
   }
 }
