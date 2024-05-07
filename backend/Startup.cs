58fedbef09f80755ab3e5eb3bdfbcba8415f8312
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2024-05-07 13:22:40 -0700

    forgot to updater configuration variable

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -28,7 +28,7 @@ namespace shoppingList.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = Configuration.GetConnectionString("cart2go")!;
+            string connectionString = _configuration.GetConnectionString("cart2go")!;
 
             services.AddScoped<IDbConnection>(provider => new SqlConnection(connectionString));
 
