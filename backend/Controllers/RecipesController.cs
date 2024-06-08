d15f765cbc5fc23764b3acb83724a7880f0006f5
TrevorStreng <99687886+TrevorStreng@users.noreply.github.com>
2024-06-07 17:27:03 -0700

    testing west instance

--- backend/Controllers/RecipesController.cs
+++ backend/Controllers/RecipesController.cs
@@ -1,43 +1,43 @@
-﻿using System.Data;
-using backend.Models;
-using Dapper;
-using Microsoft.AspNetCore.Mvc;
+﻿// using System.Data;
+// using backend.Models;
+// using Dapper;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace backend.Controllers
-{
-    [ApiController]
-  [Route("[controller]")]
+// namespace backend.Controllers
+// {
+//     [ApiController]
+//   [Route("[controller]")]
 
-  public class RecipesController : ControllerBase
-  {
-    private readonly IDbConnection _dbConnection;
+//   public class RecipesController : ControllerBase
+//   {
+//     private readonly IDbConnection _dbConnection;
 
-    public RecipesController(IDbConnection dbConnection) {
-      _dbConnection = dbConnection;
+//     public RecipesController(IDbConnection dbConnection) {
+//       _dbConnection = dbConnection;
 
-    }
+//     }
 
-    [HttpGet]
-    public async Task<IActionResult> GetAllRecipes() {
-      try {
-        string queryIgredients = "SELECT r.Id, r.name as RecipeName, i.name as IngredientName, ri.Quantity, ri.QuantityType FROM RecipeIngredients ri INNER JOIN Ingredients i on ri.IngredientId = i.Id INNER JOIN Recipes r on ri.IngredientId = i.Id ORDER by r.Id";
-        var recipeIngredients = await _dbConnection.QueryAsync(queryIgredients);
+//     [HttpGet]
+//     public async Task<IActionResult> GetAllRecipes() {
+//       try {
+//         string queryIgredients = "SELECT r.Id, r.name as RecipeName, i.name as IngredientName, ri.Quantity, ri.QuantityType FROM RecipeIngredients ri INNER JOIN Ingredients i on ri.IngredientId = i.Id INNER JOIN Recipes r on ri.IngredientId = i.Id ORDER by r.Id";
+//         var recipeIngredients = await _dbConnection.QueryAsync(queryIgredients);
 
-        string querySteps = "SELECT r.Id, r.name as RecipeName, s.Description as steps from Recipes r Inner JOIN Steps s on r.Id = s.RecipeId";
-        var recipeSteps = await _dbConnection.QueryAsync(querySteps);
+//         string querySteps = "SELECT r.Id, r.name as RecipeName, s.Description as steps from Recipes r Inner JOIN Steps s on r.Id = s.RecipeId";
+//         var recipeSteps = await _dbConnection.QueryAsync(querySteps);
 
-        buildRecipes(recipeIngredients, recipeSteps);
+//         buildRecipes(recipeIngredients, recipeSteps);
 
-        return Ok(new {recipeIngredients, recipeSteps});
-      } catch(Exception ex) {
-        return BadRequest(ex.Message);
-      }
-    }
+//         return Ok(new {recipeIngredients, recipeSteps});
+//       } catch(Exception ex) {
+//         return BadRequest(ex.Message);
+//       }
+//     }
 
-    public RecipeDto buildRecipes(IngredientsDto ingredient, StepsDto steps) {
+//     public RecipeDto buildRecipes(IngredientsDto ingredient, StepsDto steps) {
 
-    }
-  }
+//     }
+//   }
 
-}
+// }
 
